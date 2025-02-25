24cadb2b2f337eac91b0f3fa17269e42e47c8691
MickXie <mick.xie.club@gmail.com>
2025-02-26 00:45:35 +0800

    new inpute system complete

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    private GameControl gamecontrol;
+    void Awake()
+    {
+        gamecontrol = new GameControl();
+        gamecontrol.Player.Enable();
+        //啟用新inpute
+    }
     public Vector3 GetMovement()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        Vector2 inputVector2 = gamecontrol.Player.Move.ReadValue<Vector2>();
+        Vector3 direction = new Vector3(inputVector2.x, 0, inputVector2.y);
         //前後左右的移動變數設置
         direction = direction.normalized;
         //單位向量化，使左上右上更準確
