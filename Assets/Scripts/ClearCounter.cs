091a77ba13d5ca887fa8e5afc1c3b8a51658c83d
MickXie <mick.xie.club@gmail.com>
2025-03-13 01:00:52 +0800

    Transform file form , generate food (start)

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private GameObject selectedCounter;
+    [SerializeField] private GameObject kitchenObjectPrefab;
+    [SerializeField] private Transform TopPoint;
     public void Interact()
     {
         print(this.gameObject + "is interacting......");
+        GameObject go = GameObject.Instantiate(kitchenObjectPrefab, TopPoint);
+        go.transform.localPosition = Vector3.zero;
     }
     public void SelectCounter()
     {
