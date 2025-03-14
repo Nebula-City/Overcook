b00192c61617d20f65f012250292ea0744ed1d5a
MickXie <mick.xie.club@gmail.com>
2025-03-15 00:17:15 +0800

    kitchenObject interact complete half

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private GameObject selectedCounter;
-    [SerializeField] private GameObject kitchenObjectPrefab;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform TopPoint;
+    [SerializeField] private bool testing;
+    [SerializeField] private ClearCounter transferTargetCounter;
+    private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        if (testing && Input.GetMouseButtonDown(0))
+        {
+
+            TransferKitchenObject(this, transferTargetCounter);
+        }
+    }
     public void Interact()
     {
+        if (kitchenObject == null)
+        {
+            kitchenObject = GameObject.Instantiate(kitchenObjectSO.prefab, TopPoint).GetComponent<KitchenObject>();
+            kitchenObject.transform.localPosition = Vector3.zero;
+        }
+        else
+        {
+            Debug.LogWarning("已經有了-" + gameObject);
+            Destroy(kitchenObject.gameObject);
+        }
         print(this.gameObject + "is interacting......");
-        GameObject go = GameObject.Instantiate(kitchenObjectPrefab, TopPoint);
-        go.transform.localPosition = Vector3.zero;
+
     }
     public void SelectCounter()
     {
@@ -19,5 +40,37 @@ public class ClearCounter : MonoBehaviour
     public void CancelSelect()
     {
         selectedCounter.SetActive(false);
+    }
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+    public void TransferKitchenObject(ClearCounter sourceCounter, ClearCounter targetCounter)
+    {
+        if (sourceCounter.GetKitchenObject() == null)
+        {
+            Debug.LogWarning("沒有東西可以轉移");
+            return;
+        }
+        if (targetCounter.GetKitchenObject() != null)
+        {
+            Debug.LogWarning("目標位置已經有東西了");
+            return;
+        }
+        targetCounter.AddKitchenObject(sourceCounter.GetKitchenObject());
+        sourceCounter.ClearKitchenObject();
+    }
+    public void AddKitchenObject(KitchenObject kitchenObject)
+    {
+        kitchenObject.transform.SetParent(TopPoint);
+        kitchenObject.transform.localPosition = Vector3.zero;
+        this.kitchenObject = kitchenObject;
+    }
+    public void ClearKitchenObject()
+    {
+        if (kitchenObject != null)
+        {
+            this.kitchenObject = null;
+        }
     }
 }
---
+++ Assets/Scripts/KitchenObjectSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Android;
+
+[CreateAssetMenu()]
+public class KitchenObjectSO : ScriptableObject
+{
+    public GameObject prefab;
+    public Sprite sprite;
+    public string ObjectName;
+}
